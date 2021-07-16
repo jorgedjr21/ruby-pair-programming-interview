b7ecced306442f8e0f008592d5b66dc949577dd7
Jorge Junior <jorgedjr21@gmail.com>
2021-07-16 10:33:17 -0300

    allow only valid tenants to access the api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :check_tenant_api_key
+
+  private
+
+  def check_tenant_api_key
+    tenant = Tenant.find_by(api_key: request.headers['Auth'])
+
+    render json: { message: 'You are not authorized to perform this action. Please, provide a valid api_key' }, status: :unauthorized if tenant.blank?  
+  end
 end
--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -4,38 +4,59 @@ RSpec.describe "Questions Request", type: :request do
   let!(:questions) { create_list(:question, 5) }
   let!(:private_question) { create(:question, is_private: true) }
   let!(:public_question) { create(:question) }
-
+  
   let!(:answer) { create(:answer, question: public_question) }
   let(:json_response) { JSON.parse(response.body) }
   
   describe "GET /questions" do
-    subject { get '/questions', params: {} }
-    
-    context "when questions exists" do
-      it 'should not include the private questions' do
-        subject
+    subject { get '/questions', params: {}, headers: { 'Auth': api_key } }
 
-        expect(json_response).to_not include(private_question.to_json)
+    context 'with valid api_key' do
+      let(:tenant) { create(:tenant) }
+      let(:api_key) { tenant.api_key }
+
+      context "when questions exists" do
+        it 'must have an :ok status' do
+          subject
+          
+          expect(response).to have_http_status(:ok)
+        end
+
+        it 'should not include the private questions' do
+          subject
+
+          expect(json_response).to_not include(private_question.to_json)
+        end
+
+        it 'must have the public questions' do
+          subject
+          serialized_questions = questions.map { |question| QuestionSerializer.new(question).to_json }.first
+          expect(json_response).to include(JSON.parse(serialized_questions))
+        end
+
+        it 'must have the question with the answer' do
+          subject
+          question = QuestionSerializer.new(public_question).to_json
+
+          expect(json_response).to include(JSON.parse(question))
+        end
+
+
+        it 'must have the question name' do
+          subject
+
+          expect(json_response.first.keys).to include('title')
+        end
       end
+    end
 
-      it 'must have the public questions' do
-        subject
-        serialized_questions = questions.map { |question| QuestionSerializer.new(question).to_json }.first
-        expect(json_response).to include(JSON.parse(serialized_questions))
-      end
-
-      it 'must have the question with the answer' do
-        subject
-        question = QuestionSerializer.new(public_question).to_json
-
-        expect(json_response).to include(JSON.parse(question))
-      end
-
-
-      it 'must have the question name' do
+    context 'with wrong api key' do
+      let(:api_key) { 'wrong_api_key' }
+  
+      it 'must have an :unauthorized http status' do
         subject
 
-        expect(json_response.first.keys).to include('title')
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
